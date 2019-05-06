64bea280bd50406ec3ba68176af246ac458e2e87
Lianne Haug <liannehaug@L-156180773.local>
2019-05-06 15:04:02 -0600

    Add linter

--- src/tasks/index.js
+++ src/tasks/index.js
@@ -1,17 +1,9 @@
-import { test } from '../utils/database';
+// import { getBookshelf } from '../utils/database';
 
-desc('Scrape routes');
-task('scrape_routes', function () {
-  console.log("HI");
+desc('Scrape routes'); // eslint-disable-line
+task('scrape_routes', function () { // eslint-disable-line
 });
 
-desc('Scrape upgrade data for relevant routes departing within 30 minutes');
-task('scrape_upgrades', function (a, b, c) {
-  console.log('SCRAPE');
-});
-
-
-desc('Scrape upgrade data for relevant routes departing within 30 minutes');
-task('default', function (a, b, c) {
-  console.log('SCRAPE');
+desc('Scrape upgrade data for relevant routes departing within 30 minutes'); // eslint-disable-line
+task('scrape_upgrades', function () { // eslint-disable-line
 });
--- src/utils/database.js
+++ src/utils/database.js
@@ -4,17 +4,17 @@ const knex = require('knex')({
   client: 'pg',
   version: '11.2',
   connection: {
-    host : '127.0.0.1',
-    user : process.env.DATABASE_USER,
-    password : process.env.DATABASE_PASSWORD,
-    database : process.env.DATABASE_NAME
-  }
+    host: '127.0.0.1',
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+  },
 });
+const bookshelf = require('bookshelf')(knex);
 
-export const getBookshelf = () => {
 
-}
+export const getBookshelf = () => bookshelf;
 
 export const closeConnection = () => {
 
-}
+};
